68733edf18a921ae26f7ad6b43439c7502bbb5bb
yangshenghua <2103025644@qq.com>
2020-04-22 17:36:04 +0800

    commit

--- src/main/java/TheArtConcurrent/chapter1/ConcurrencyTest.java
+++ src/main/java/TheArtConcurrent/chapter1/ConcurrencyTest.java
@@ -26,7 +26,9 @@ public class ConcurrencyTest {
         long time = System.currentTimeMillis()-start;
         thread.join();
         System.out.println("concurrency:"+time+"ms,b = "+b);
+        System.out.println("yanghsenghua");
     }
+
     private static void serial(){
         long start = System.currentTimeMillis();
         int a = 0;
