c0d2432f8c701b44615ff6eab95c32e0cad292d7
yangshenghua <2103025644@qq.com>
2020-05-05 17:31:55 +0800

    commit

--- src/main/java/demo/demo01/CallableDemo.java
+++ src/main/java/demo/demo01/CallableDemo.java
@@ -1,12 +1,13 @@
 package demo.demo01;
 
+
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class CallableDemo {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        CallDemoImpl ci = new CallDemoImpl();
-        FutureTask<Integer> ft = new FutureTask<>(ci);
+        CallDemoImpl callimpl = new CallDemoImpl();
+        FutureTask<Integer> ft = new FutureTask<>(callimpl);
         Thread thread = new Thread(ft);
         thread.start();
         System.out.println(ft.get());
